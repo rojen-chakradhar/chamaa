5cc35f54fc343fd2e694b8f3a53438dca66e52df
Rojen Chakradhar <chakradhar@rojen.me>
2026-06-22 02:30:08 +0545

    item page making

--- scripts/script.js
+++ scripts/script.js
@@ -4,47 +4,41 @@ const variety = [
     desc: "tasty",
     price: "Rs 200",
     image: "https://nakednutrition.com/cdn/shop/articles/Depositphotos_8628296_S_2000x.jpg?v=1689059503",
+    src: "fudgy",
   },
   {
     name: "Cakey Brownie",
     desc: "delicous",
     price: "Rs 500",
     image: "https://www.soulfullymade.com/wp-content/uploads/2023/03/cake-brownies-recipe-square-featured.jpg",
+    src: "cakey",
   },
   {
     name: "Brownie Bites",
     desc: "tasty",
     price: "Rs 50",
     image: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/11/11/REE_DRUMMOND_DARK_CHOCOLATE_BROWNIE_BITES_H.jpg.rend.hgtvcom.1280.1280.suffix/1636679032846.webp",
+    src: "bites",
   },
   {
     name: "Red Velvet Brownie",
     desc: "tasty",
     price: "Rs 250",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKIt9CtMu4xc-SSS4yib7Wadx7fW1g1JEbhxCqXs-DgbP1oHQWVCMYBSDR&s=10",
+    src: "red-velvet",
   },
 ]
-
-let grid = document.getElementById("menuGrid");
-
+let grid = document.querySelector("#menuGrid");
 variety.forEach(type => {
-  grid.innerHTML = `
-    <div class="item">
-      <img src="${type.image}" alt="${type.name}'s image">
-      <h3>${type.name}</h3>
-      <div class="item-bottom">
-        <p>${type.price}</p>
-        <i class="ri-shopping-cart-line"></i>
-      </div>
-    </div>`;
-  // grid.forEach(el => {
-  //   // el.innerHTML=`<h2>${elm.name}</h2><p>${elm.desc}</p><p>${elm.price}</p>`;
-  //   el.addEventListener("click", () => {
-  //     window.location.href = "http://127.0.0.1:5500/item.html"
-  //     localStorage.setItem("name", elm.name);
-  //     localStorage.setItem("description", elm.desc);
-  //     localStorage.setItem("price", elm.price);
-  //     localStorage.setItem("image", elm.image);
-  //   })
-  // });
+  grid.innerHTML += `
+  <a href="./${type.src}.html">
+  <div class="item">
+    <img src="${type.image}" alt="${type.name}'s image">
+    <h3>${type.name}</h3>
+    <div class="item-bottom">
+      <p>${type.price}</p>
+      <i class="ri-shopping-cart-line"></i>
+    </div>
+    </div>
+  </a>`;
 });
